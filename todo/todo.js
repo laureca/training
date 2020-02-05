5778b10b6fbf88c7cb32b57cab4af4710b32e47d
laureca <anynnha.laureca@gmail.com>
2020-02-05 17:40:20 -0300

    added JS document and initialization of some variables

---
+++ todo/todo.js
@@ -0,0 +1,4 @@
+var novoItem = document.getElementById("novo-item"); // new-task
+var plusBotao = document.getElementsByTagName("button")[0];//first button
+var paraFazer = document.getElementById("para-fazer"); //incomplete-tasks
+var completado = document.getElementById("completado"); //completed-tasks
