b0f1ecf4c7aec3cdc27077ec4cbaf4a1b58392ad
laureca <anynnha.laureca@gmail.com>
2020-02-05 18:16:20 -0300

    css modified

--- todo/todo.js
+++ todo/todo.js
@@ -3,7 +3,7 @@ var plusBotao = document.getElementsByTagName("button")[0];//first button
 var paraFazer = document.getElementById("para-fazer"); //incomplete-tasks
 var completado = document.getElementById("completado"); //completed-tasks
 
-function novoElemento(itemString) {
+var newElement = function novoElemento(itemString) {
     var elemento = document.createElement('li');
     var checkbox = document.createElement('input');
     var label = document.createElement('label');
