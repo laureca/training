f416669654e877e4820d29df9c99aa2fee2cb27a
laureca <anynnha.laureca@gmail.com>
2020-02-06 10:37:49 -0300

    completion of the element creation function

--- todo/todo.js
+++ todo/todo.js
@@ -1,9 +1,12 @@
-var novoItem = document.getElementById("novo-item"); // new-task
-var plusBotao = document.getElementsByTagName("button")[0];//first button
-var paraFazer = document.getElementById("para-fazer"); //incomplete-tasks
-var completado = document.getElementById("completado"); //completed-tasks
+var novoItem = document.getElementById('novo-item').value; // string do novo item
+var plusBotao = document.getElementsByTagName('button')[0]; // primeiro botão - de add
+var paraFazer = document.getElementById('para-fazer'); // itens para fazer
+var completado = document.getElementById('completado'); // itens completados
 
-var newElement = function novoElemento(itemString) {
+function novoElemento(novoItem) {
+
+    console.log("entrou na função de add");
+
     var elemento = document.createElement('li');
     var checkbox = document.createElement('input');
     var label = document.createElement('label');
@@ -11,15 +14,15 @@ var newElement = function novoElemento(itemString) {
     var botaoEditar = document.createElement('button');
     var botaoDeletar = document.createElement('button');
 
-    checkbox.type = "checkBox";
-    texto.type = "text";
+    checkbox.type = 'checkBox';
+    texto.type = 'text';
 
-    botaoEditar.innerText = "Editar";
-    botaoDeletar.innerText = "Deletar";
-    label.innerText = itemString;
+    botaoEditar.innerText = 'Editar';
+    botaoDeletar.innerText ='Deletar';
+    label.innerText = novoItem;
 
-    botaoEditar.className = "edit";
-    botaoDeletar.className = "delete";
+    botaoEditar.className = 'edit';
+    botaoDeletar.className = 'delete';
 
     elemento.appendChild(checkbox);
     elemento.appendChild(label);
@@ -27,5 +30,7 @@ var newElement = function novoElemento(itemString) {
     elemento.appendChild(botaoEditar);
     elemento.appendChild(botaoDeletar);
 
-    return elemento;
+    console.log(elemento)
+
+    paraFazer.appendChild(elemento);
 }
